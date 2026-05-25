8047af9dece5842db940745306a04d651eab0858
minnie-star <minenhleminnie8@gmail.com>
2026-05-26 01:05:16 +0200

    Oauth setup

--- server.js
+++ server.js
@@ -1,31 +1,76 @@
 require('dotenv').config();
 const express = require('express');
+const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 const connectDB = require('./database/database');
+const passport = require('passport');
+const session = require('express-session');
+const GitHubStrategy = require('passport-github').Strategy;
 
 const userRoutes = require('./routes/userRoutes');
 const readingRoutes = require('./routes/readingRoutes');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger_output.json');
 
-
-
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(bodyparser.json());
 app.use(express.json());
 app.use(cors());
 app.use(morgan('dev'));
-
+app.use(passport.initialize());
+app.use(session()); 
 // Connect to MongoDB
 connectDB();
 
 // Routes
 app.use('/api/users', userRoutes);
 app.use('/api/readings', readingRoutes);
+
+app.use('/api/auth/github', require('./routes/authRoutes')); // GitHub OAuth routes
+
+// CORS headers
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origgin, X-Rrequested-With, Content-Type, Aaccept, Z-Key, Authorization");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, OPTIONS, DELETE");
+    next();
+});
+
+app.use(cors({ methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH']}));
+app.use(cors({ origin: '*'}));
+
+// GitHub OAuth setup
+passport.use(new GitHubStrategy({
+    clientID: process.env.GITHUB_CLIENT_ID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    callbackURL: process.env.GITHUB_CALLBACK_URL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    return done(null, profile);
+  }
+));
+
+// Passport session setup
+passport.serializeUser((user, done) => {
+    done(null,user);
+});
+
+passport.deserializeUser((user, done) => {
+    done(null, user);
+});
+
+app.get('/', (req, res) => { res.send(req.session.user !== undefined ? `Logged in as ${req.session.user.displayName}` : 'Not logged in') });
+
+app.get('/github/callback', passport.authenticate('github', {
+    failureRedirect: '/api-docs', session: false}),
+    (req, res) => {
+        req.session.user =req.user;
+        res.redirect('/');
+    });
 
 // Swagger API docs
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
