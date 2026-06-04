85868608ff12250d41bf936de8198d0efbd3153d
minnie-star <minenhleminnie8@gmail.com>
2026-06-04 12:57:09 +0200

    debug

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'https://glucometer-tracker.onrender.com',
+  credentials: true
+}));
 app.use(morgan('dev'));
 app.use(session({
   secret: process.env.SESSION_SECRET || 'supersecret',
